d595aa14daa1c8aa47ace8b0b8b67a5d098e6e52
Cambaza <Cambaza@users.noreply.github.com>
2021-12-29 18:37:55 +0800

    Update additional_info.py

--- apps/additional_info.py
+++ apps/additional_info.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+
 import yfinance as yf
 
 def app(params):
