e5c0399a68dc1a395e779c9f2af72f97ebeda261
Cambaza <Cambaza@users.noreply.github.com>
2021-12-29 18:46:17 +0800

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -36,7 +36,7 @@ class MultiApp:
             start_date = st.sidebar.date_input('Start Date:', value=pd.to_datetime("2021-01-31", format="%Y-%m-%d"))
             end_date = st.sidebar.date_input('End Date:', value=pd.to_datetime("today", format="%Y-%m-%d"))
             
-            interv = st.sidebar.selectbox('Select Interval', ['1m', '5m','1h','1d','1wk','1mo','3mo' ])
+            interv = st.sidebar.selectbox('Select Interval', ['1h','1d','1wk','1mo','3mo' ])
             custom = {'start_date': start_date, 'end_date': end_date, 'currency': curr, 'interval': interv}
 
         params = {'period': option, 'currency': curr}
